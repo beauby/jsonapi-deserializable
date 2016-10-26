c5c468bfcbb8308be58311f4740033dc36fb49b2
Lucas Hosseini <lucas.hosseini@gmail.com>
2016-10-26 02:26:19 +0200

    Fix relationships DSL.

--- lib/jsonapi/deserializable/relationship_dsl.rb
+++ lib/jsonapi/deserializable/relationship_dsl.rb
@@ -7,12 +7,10 @@ module JSONAPI
 
       module ClassMethods
         def has_one(&block)
-          block ||= proc { |rel| field key.to_sym => rel }
           self.has_one_block = block
         end
 
         def has_many(&block)
-          block ||= proc { |rel| field key.to_sym => rel }
           self.has_many_block = block
         end
       end
